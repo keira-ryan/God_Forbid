c1d170878e82caba1b5ee0dc38b795470ca9e853
Keira Ryan <keiryan@chapman.edu>
2025-10-27 21:13:07 -0700

    Updated hitbox and hurtbox functionality!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,33 @@
+using DG.Tweening;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private DialogueUI dialogueUI;
     
+    public CinemachineCamera vCamera;
+    public float offsetAmount = 1f;
+    public CinemachinePositionComposer positionComposer;
     public DialogueUI DialogueUI => dialogueUI;
     
     public IInteractable Interactable { get; set; }
     
     private Rigidbody2D rb;
+    private float baseOffsetX;
+
+    private bool FacingLeft = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    void Awake()
+    {
+        baseOffsetX = positionComposer.TargetOffset.x;
+    }
+    
 
     void Update()
     {
@@ -24,4 +38,55 @@ public class Player : MonoBehaviour
             Interactable?.Interact(this);
         }
     }
+
+    void FixedUpdate()
+    {
+        TurnCheck();
+    }
+
+    private void TurnCheck()
+    {
+        float moveDir = rb.linearVelocity.x; 
+
+        if (moveDir > 0.01f && FacingLeft)
+        {
+            FacingLeft = false;
+            Flip();
+        }
+        else if (moveDir < -0.01f && !FacingLeft)
+        {
+            FacingLeft = true;
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y += 180f;
+        transform.eulerAngles = rotation;
+        //TweenOnTurn();
+    }
+
+    /*
+    private void TweenOnTurn()
+    {
+        float targetOffsetX = FacingLeft 
+            ? baseOffsetX - Mathf.Abs(offsetAmount) 
+            : baseOffsetX + Mathf.Abs(offsetAmount);
+
+        float currentOffsetX = positionComposer.TargetOffset.x;
+
+        DOTween.To(
+            () => currentOffsetX,
+            x =>
+            {
+                currentOffsetX = x;
+                positionComposer.TargetOffset.x = currentOffsetX;
+            },
+            targetOffsetX,
+            0.25f
+        ).SetEase(Ease.OutSine);
+    }
+    */
 }
