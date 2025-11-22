750aefdc1986285c26b938bf892450271abd4fba
Keira Ryan <keiryan@chapman.edu>
2025-11-21 23:53:04 -0800

    Final touches

--- Assets/Scripts/ScreenScripts/UIController.cs
+++ Assets/Scripts/ScreenScripts/UIController.cs
@@ -13,6 +13,11 @@ public class UIController : MonoBehaviour
     public GameObject pauseScreen;
     public GameObject playUI;
     public TextMeshProUGUI controlsUI;
+    public GameObject gameWonScreen;
+    public GameObject gameLostScreen;
+    
+    public SchopenhauerAI schopenhauerAI;
+    public GameController gameController;
 
     private void Awake()
     {
@@ -32,6 +37,8 @@ public class UIController : MonoBehaviour
         pauseScreen.SetActive(false);
         endScreen.SetActive(false);
         controlsUI.gameObject.SetActive(false);
+        gameWonScreen.SetActive(false);
+        gameLostScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -97,12 +104,37 @@ public class UIController : MonoBehaviour
     private void OnEnable()
     {
         Player.OnHealingGranted += ShowHealInstruction;
+        Player.OnDeath += ShowGameLostScreen;
+        SchopenhauerAI.OnDeath += ShowGameWonScreen;
+    }
+
+    private void ShowGameWonScreen()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        playUI.SetActive(false);
+        gameWonScreen.SetActive(true);
+        pauseScreen.SetActive(false);
+        startScreen.SetActive(false);
+        endScreen.SetActive(false);
     }
 
     private void OnDisable()
     {
         Player.OnHealingGranted -= ShowHealInstruction;
+        Player.OnDeath -= ShowGameLostScreen;
+        SchopenhauerAI.OnDeath -= ShowGameWonScreen;
     }
-    
-    
+
+    private void ShowGameLostScreen()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        gameLostScreen.SetActive(true);
+        gameWonScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+        startScreen.SetActive(false);
+        playUI.SetActive(false);
+        endScreen.SetActive(false);
+    }
 }
